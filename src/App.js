ad1d7c07c066c6480816948fb953b0acaddac2c8
Elena <karengvcs@gmail.com>
2020-09-22 17:03:05 -0500

    Add configuration to use apollo dependecy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import GetAllPeople from './graphql/queries'
 
 function App() {
   return (
     <div className="App">
-     fdfsdf
+     <GetAllPeople/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri:"https://swapi-graphql.netlify.app/.netlify/functions/index",  
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client = {client} >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
