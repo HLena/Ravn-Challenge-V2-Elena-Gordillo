08cdee4246ec03f0850730cf1e6133369bb4b945
Elena <karengvcs@gmail.com>
2020-09-25 21:37:35 -0500

    Pass a onLoadMore function
    Allows to make the pagination of the elements and showing it 5 by 5

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import PersonCell from './PersonCell'
 
-const PeopleList = ({entries, onLoadMore, networkStatus}) => {
+import { Link } from 'react-router-dom';
+
+const PeopleList = ({entries, onLoadMore, networkStatus,loading}) => {
   return (
     <>
         {
-          entries.map(({node}) => {
-           return (
-             <PersonCell 
-              key={node.id} 
-              node={node}
-              />
-           )
-          },onLoadMore())
+          entries.map(({node}, idx) => {
+            return (
+            <Link className="link" key={idx} to={`/${node.id}`}>
+              <PersonCell 
+                key={node.id} 
+                node={node}
+                />
+            </Link>
+          
+            )},onLoadMore())
         }
-      {networkStatus === 3 ? <div>Loading</div> : null}
-
     </>
   )
 }
