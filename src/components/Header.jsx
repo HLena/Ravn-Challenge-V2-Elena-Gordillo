c95de9adc9a7bd3624ef6af7d2da0147e50bff9c
Elena <karengvcs@gmail.com>
2020-09-25 21:27:27 -0500

    Chore: Create component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className="nav-bar">
+      <h2 className="text" >People of Stars  Wars</h2>
+    </header>
+  )
+}
+
+export default Header;
