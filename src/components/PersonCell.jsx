350cdeb0ae91afc800313c568e115cf749a6c08b
Elena <karengvcs@gmail.com>
2020-09-25 21:39:30 -0500

    Render the name, specie and homeworld

--- src/components/PersonCell.jsx
+++ src/components/PersonCell.jsx
@@ -4,6 +4,8 @@ import vector from '../svg/Vector.svg'
 export const PersonCell = ({node}) => {
   const {name, homeworld, species} = node;
   const specie = species ? species.name : 'Human';
+  const handleClick = () => { 
+  }
   return (
     <>
       <div className="person-cell">
@@ -11,7 +13,7 @@ export const PersonCell = ({node}) => {
         <h2 className="text">{name}</h2>
         <p className="text">{`${specie} from ${homeworld.name}`}</p>
         </div>
-        <img src={vector} alt=""/>
+        <img src={vector} alt="" onClick={handleClick}/>
       </div>
       <hr className = "line"/>
     </>
